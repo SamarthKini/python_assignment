f9eaddadf24e53efcd05f716deb1157090df4797
SamarthKini <samarthkini@outlook.com>
2024-11-17 00:22:48 +0530

    commit - 13

--- 13.py
+++ 13.py
@@ -1,8 +1,12 @@
 # 13. Write a Python function called count_vowels that takes a string as input and returns the count of vowels (a, e, i, o, u) in the string.
-vowels = ["a", 'e', 'i', 'o', 'u']
+
+def count_vowels(string) -> int:
+    vowels = ["a", 'e', 'i', 'o', 'u']
+    count = 0
+    for i in string:
+        if i in vowels:
+            count += 1
+    return count
+
 string = input("Enter the string: ")
-count = 0
-for i in string:
-    if i in vowels:
-        count += 1 
-print(f"Vowel count = {count}")
+print(f"Vowel count = {count_vowels(string)}")
