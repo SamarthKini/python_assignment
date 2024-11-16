f227e638458e41c5882b77a61179d88e2da193f9
SamarthKini <samarthkini@outlook.com>
2024-11-16 14:02:42 +0530

    commit - 11

---
+++ 11.py
@@ -0,0 +1,3 @@
+# 11. Ask the user to enter a sentence. Convert this sentence into a tuple of words, then displayeach word on a new line along with its position in the sentence.
+sentence = input("Enter the sentece: ").split()
+print((tuple(sentence)))
