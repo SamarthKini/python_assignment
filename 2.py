b402957e54b0a4a0f76806cb435a14afabf64f78
SamarthKini <samarthkini@outlook.com>
2024-11-16 12:06:57 +0530

    commit 2

---
+++ 2.py
@@ -0,0 +1,6 @@
+#2. Create a program that takes two string inputs from the user, concatenates them, and then repeats the concatenated string five times.
+
+
+s1 = input("Enter the string 1: ")
+s2 = input("Enter the string 2: ")
+print(f"Concatenated string: {s1 + s2}")
